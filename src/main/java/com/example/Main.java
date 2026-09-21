15bcc8b04b34de8ee5371efb645f3dc5dfb2eaa7
CNaveen86509 <2022wb86509@wilp.bits-pilani.ac.in>
2026-09-21 18:44:22 +0530

    Task 4: Added CI test message

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,5 +3,6 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, DevOps Lab!");
+        System.out.println("Hello Jenkins CI Task 4!");
     }
 }
